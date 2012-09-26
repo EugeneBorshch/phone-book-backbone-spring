9a21168fca60e2b0981171273a4e94a37ddc243c
Eugene Borshch <st.vinsent@gmail.com>
2012-09-26 22:50:00 +0300

    fucking with backbone

--- src/main/webapp/js/phone-book.js
+++ src/main/webapp/js/phone-book.js
@@ -19,6 +19,22 @@ $(function () {
                     email:"No email no cry",
                     phone:"No phone no cry"
                 };
+            },
+
+            validation:{
+                name:{
+                    required:true,
+                    msg:'Please enter a name'
+                },
+                phone:{
+                    required:true,
+                    pattern:'number',
+                    msg:'Please enter a valid phone number'
+                },
+                email:{
+                    pattern:'email',
+                    msg:'Please enter a valid email'
+                }
             }
         }
     ); //Backbone.Model.extend end
@@ -28,10 +44,10 @@ $(function () {
 
     var ContactCollection = Backbone.Collection.extend(
         {
-            model: Contact,
-            localStorage: new Backbone.LocalStorage("phone-backbone"),
+            model:Contact,
+            localStorage:new Backbone.LocalStorage("phone-backbone"),
 
-            comparator: function (contact) {
+            comparator:function (contact) {
                 return contact.get("name");
             }
         }
@@ -43,32 +59,32 @@ $(function () {
      **********************************************************************************/
     var ContactListView = Backbone.View.extend(
         {
-            el: $("#contact-list-block"), // DOM element with contact list
+            el:$("#contact-list-block"), // DOM element with contact list
 
 
-            template: _.template($('#contact-list-template').html()) ,
+            template:_.template($('#contact-list-template').html()),
 
-            initialize: function() {
+            initialize:function () {
                 this.model.on('change', this.render, this);
-               // this.model.on('destroy', this.remove, this);
+                // this.model.on('destroy', this.remove, this);
             },
 
-            events: {
-                "click .entry": "openContact"
+            events:{
+                "click .entry":"openContact"
             },
 
 
-            render: function(){
-                $(this.el).html(this.template({contact : this.model.toJSON()}));
+            render:function () {
+                $(this.el).html(this.template({contact:this.model.toJSON()}));
                 return this;
-            } ,
+            },
 
-            openContact: function(e){
+            openContact:function (e) {
 
                 e.preventDefault();
                 var id = $(e.currentTarget).data("id");
                 var item = this.model.get(id);
-                var contactView = new ContactDetailsView({model: item});
+                var contactView = new ContactDetailsView({model:item});
                 contactView.render();
             }
         }
@@ -77,35 +93,117 @@ $(function () {
 
     var ContactDetailsView = Backbone.View.extend(
         {
-            el: $("#contact-details-block"), // DOM element with contact details
+            el:$("#contact-details-block"), // DOM element with contact details
 
-            template: _.template($('#contact-details-template').html()) ,
 
-            render: function(){
-                $(this.el).html(this.template({contact : this.model.toJSON()}));
+            template:_.template($('#contact-details-template').html()),
+
+            events:{
+                "blur input":"updateContact"
+            },
+
+            render:function () {
+                $(this.el).html(this.template({contact:this.model.toJSON()}));
+
+                Backbone.Validation.bind(this, {
+                    valid:function (view, attr) {
+                        var control;
+                        if (attr == 'name')
+                            control = $('#inputName');
+                        var control;
+                        if (attr == 'phone')
+                            control = $('#inputPhone');
+                        var control;
+                        if (attr == 'email')
+                            control = $('#inputEmail');
+                        if (control != undefined) {
+                            var group = control.parents(".control-group");
+                            group.removeClass("error");
+                        }
+                    },
+                    invalid:function (view, attr, error, selector) {
+                        var control;
+                        if (attr == 'name')
+                            control = $('#inputName');
+                        var control;
+                        if (attr == 'phone')
+                            control = $('#inputPhone');
+                        var control;
+                        if (attr == 'email')
+                            control = $('#inputEmail');
+                        if (control != undefined) {
+                            var group = control.parents(".control-group");
+                            group.addClass("error");
+                        }
+
+                    }
+                });
                 return this;
+            },
+
+            updateContact:function (e) {
+                e.preventDefault();
+                var newName = this.$("#inputName").val();
+                var newPhone = this.$("#inputPhone").val();
+                var newEmail = this.$("#inputEmail").val();
+                 console.log(this.model.toJSON());
+                this.model.save({name:newName, phone:newPhone, email:newEmail });
+
             }
         }
     );
 
+    /**********************************************************************************
+     **                     Router
+     **********************************************************************************/
 
 
+
+//    var Controller = Backbone.Router.extend({
+//        routes: {
+//            "": "list",
+//            "!/": "list",
+//            "!/add": "add"
+//        },
+//
+//        start: function () {
+//            $(".block").hide();
+//            $("#start").show();
+//        },
+//
+//        success: function () {
+//            $(".block").hide();
+//            $("#success").show();
+//        }
+//
+//
+//    });
+
+    var controller = new Controller();
+
+    Backbone.history.start();
     //TEST INIT
     var testCollection = new ContactCollection();
 
 
-    var contactView = new ContactListView({model: testCollection});
+    var contactView = new ContactListView({model:testCollection});
 
     testCollection.create({
         name:"John Smith",
-        phone:"1444 1231 1231",
+        phone:"144412311231",
         email:"john@smith.com"
     });
 
     testCollection.create({
         name:"Alison Burgers",
-        phone:"222 3333 543",
+        phone:"2223333543",
         email:"alison@burgers.com"
     });
 
-})
+    testCollection.create({
+        name:"Ben Roonie",
+        phone:"4444444",
+        email:"ben@roonie.com"
+    });
+
+});
